3ec0270f2b46d830f282228a7930af87215bf658
Jorge Aguiar Martín <jorge.aguiarmartin.external@atos.net>
2018-08-01 16:11:13 +0200

    Añadido Karma.config.js para automatizacion de testing

---
+++ karma.config.js
@@ -0,0 +1,29 @@
+// Karma configuration
+// Generated on Wed Aug 01 2018 13:45:13 GMT+0200 (GMT+02:00)
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+        'src/*.js',
+        'spec/*.js'
+    ],
+    exclude: [
+        'lib',
+        'node_modules'
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+};
